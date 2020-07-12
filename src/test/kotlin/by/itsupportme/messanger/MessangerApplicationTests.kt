4d7fd70d939a6ef13f9ea6afc97df32761700a76
Jaraxxuss <evgenij.rudenkov@mail.ru>
2020-07-12 11:23:38 +0300

    Initial commit

---
+++ src/test/kotlin/by/itsupportme/messanger/MessangerApplicationTests.kt
@@ -0,0 +1,13 @@
+package by.itsupportme.messanger
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MessangerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
